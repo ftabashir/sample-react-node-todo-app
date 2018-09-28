da36b42e0fe1a28bd2df6f94696f588501f8a90f
Farzad Tabashir <ftabashir@gmail.com>
2018-09-28 17:20:58 +0330

    read api url from ENV if available

--- client/src/graphql/index.ts
+++ client/src/graphql/index.ts
@@ -1,7 +1,9 @@
 import ApolloClient from 'apollo-boost';
 import { InMemoryCache } from 'apollo-boost'
 
-const GRAPGQL_URL = 'http://localhost:3000/graphql'
+const apiHost = process.env.API_HOST || 'localhost';
+const apiPort = process.env.API_PORT || '3000';
+const GRAPGQL_URL = `http://${apiHost}:${apiPort}/graphql`;
 export const apolloClient = new ApolloClient({
     cache: new InMemoryCache({ addTypename: false }),
     uri: GRAPGQL_URL
